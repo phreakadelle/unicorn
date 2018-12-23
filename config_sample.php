8bc809665b15f9ad2a1865e2e0b6dc8bb35a40b0
Stephan Watermeyer <m@phrk.de>
2018-12-23 12:56:03 +0100

    Digital Photframe. Digitaler Fotorahmen

--- config_sample.php
+++ config_sample.php
@@ -11,6 +11,23 @@ Config::write("key", getenv('UNICORN_KEY') ? getenv('UNICORN_KEY') : "xxxxxxxxxx
 Config::write("secret", getenv('UNICORN_SECRET') ? getenv('UNICORN_SECRET') : "xxxxxxxxxxxxxxx");
 Config::write("access_token", getenv('UNICORN_ACCESS_TOKEN') ? getenv('UNICORN_ACCESS_TOKEN') : "xxxxxxxxxxxxxxx");
 
+// The Url to the "image.php"
+Config::write("image_url", "https://xxxxxxxxxxxxxxx");
+
+// Refresh Interval
+Config::write("image_refresh", "3");
+
+// The admin URL to trigger the refresh
+Config::write("admin_url", "https://xxxxxxxxxxxxxxx");
+
+// Mail-Settings
+Config::write("mail_host", "{xxxxxxxxxxxxxxx:993/imap/ssl}INBOX");
+Config::write("mail_user", "xxxxxxxxxxxxxxx");
+Config::write("mail_password", "xxxxxxxxxxxxxxx");
+Config::write("image_cache", "20");
+Config::write("mail_image_directory_current", "/mails/");
+Config::write("mail_image_directory_archive", "/mails-archive/");
+
 // To ensure we don't attempt to obtain an access token for each user request, we must store it
 // inside a directory.
 // We also store the cursor for the delta api inside this directory.
