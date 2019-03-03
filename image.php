8894991db826f3cb1f840365b5b820781e205761
Stephan Watermeyer <m@phrk.de>
2019-03-03 10:06:16 +0100

    Ignore Video Attachments. Support for Displaying Messages on the Mobile Device

--- image.php
+++ image.php
@@ -4,7 +4,7 @@ namespace Mre\Unicorn\lib;
 require 'vendor/autoload.php';
 require_once ("config.php");
 
-function listFolderFiles($dir, $ext = "jpg"){
+function listFolderFiles($dir, $ext = "jpg,jpeg"){
     $ffs = scandir($dir);
 
     unset($ffs[array_search('.', $ffs, true)]);
@@ -15,12 +15,17 @@ function listFolderFiles($dir, $ext = "jpg"){
         return;
 
 	$retVal = array();
+	$allFileExt = explode(",", $ext);
     foreach($ffs as $ff){
-        if(strpos(strtolower($ff), $ext) && strpos($dir, "thumbs") == 0 ) {
-			$retVal[] = $dir."/".$ff;
-		}
-        if(is_dir($dir.'/'.$ff)) {
-			$retVal = array_merge($retVal, listFolderFiles($dir.'/'.$ff, $ext));
+		
+		foreach($allFileExt as $currentFileExt) {
+			if(strpos(strtolower($ff), $currentFileExt) && strpos($dir, "thumbs") == 0 ) {
+				$retVal[] = $dir."/".$ff;
+			}
+		
+			if(is_dir($dir.'/'.$ff)) {
+				$retVal = array_merge($retVal, listFolderFiles($dir.'/'.$ff, $currentFileExt));
+			}
 		}
     }
 	return $retVal;
@@ -40,6 +45,36 @@ if($current == -1) {
 	$next = 0;
 }
 $theImage = $files[$next];
+setlocale(LC_TIME, Config::read("image_locale"));
+
+// Handle Subject
+if(file_exists(Config::read("image_subject_file"))) {
+	$theMessage = "";
+	$file = fopen(Config::read("image_subject_file"), "r");
+	$knownSenders = Config::read("image_known_senders");
+	
+	while(!feof($file)) {
+		$currentLine = fgets($file);
+		$currentLine = explode(";", $currentLine);
+		$messageCreateDate = strtotime($currentLine[0]);
+		if(strtotime("-".Config::read("image_message_age")." Days") < $messageCreateDate) {
+			$date = strftime("%A, %d.%m", $messageCreateDate);
+			$from = $currentLine[1];
+			if(isset($knownSenders[$from])) {
+				$from = $knownSenders[$from];
+			}
+			$msg = $currentLine[2];
+			if($msg != "") {
+				$theMessage .= "Nachricht von ".$from." am ".$date.": ".$msg." - - - - -  ";
+			}
+		} else {
+			// this is an old message that will not be displayed
+		}
+	}
+	fclose($file);
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -49,17 +84,41 @@ $theImage = $files[$next];
 <meta http-equiv="refresh" content="<?php echo Config::read("image_refresh")?>; url=<?php echo Config::read("site_url")?>?current=<?php echo $next;?>" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <title><?php echo Config::read("gallery_name")?></title>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
 <style>
 body {background-color: black;}
 #fixed-div {    position: fixed;    top: 1em;    right: 1em; }
+
+@keyframes push {
+    from {
+        margin: 100%;
+    }
+    to {
+        margin: 0;
+    }
+}
+.marquee {
+    white-space: nowrap;
+    overflow: hidden;
+	background-color: black;
+	color: white;
+	font-size: 1.4em;
+}
+.marquee > span:nth-child(1) {
+    animation:  20s linear 0s infinite push;
+}
 </style>
 
 </head>
 
 <body>
-<div id="fixed-div">
+
+<?php if($theMessage != "") { ?>
+<div class="marquee">
+	<span><?php echo $theMessage; ?></span>
 </div>
+<?php } ?>
+
 <?php
 echo "<img src=\"".$theImage."\" width=\"100%\"/>";
 ?>
--- mail2dropbox.php
+++ mail2dropbox.php
@@ -8,9 +8,29 @@ use Kunnu\Dropbox\DropboxApp;
 use Kunnu\Dropbox\Dropbox as BaseDropbox;
 use Kunnu\Dropbox\DropboxFile as DropboxFile;
 
+
+function isVideo($pFilename) {
+    if(strlen($pFilename) == 0) {
+        return false;
+    }
+    
+    $formats = array("avi", "mpg", "mpeg", "mov");
+    
+    $retVal = false;
+    foreach($formats as $currentFormat) {
+        if($strpos($pFilenmae, ".".$currentFormat)) {
+            $retVal = true;
+        }
+    }
+    return $retVal;
+}
+
+header('Content-Type: application/json');
+
 /* return message*/
 $retVal = array();
 $retVal['debug'] = array();
+$retVal['messages'] = array();
 
 /* try to connect */
 $inbox = imap_open(Config::read('mail_host'), Config::read('mail_user'), Config::read('mail_password')) or die('Cannot connect to Email: ' . imap_last_error());
@@ -31,7 +51,7 @@ if ($emails) {
         
         /* get information specific to this email */
         $overview = imap_fetch_overview($inbox, $email_number, 0);
-        $message = imap_fetchbody($inbox, $email_number, 2);
+        //$message = imap_fetchbody($inbox, $email_number, 2);
         $structure = imap_fetchstructure($inbox, $email_number);
         
         $attachments = array();
@@ -75,40 +95,42 @@ if ($emails) {
         
         if (count($attachments) != 0) {
             foreach ($attachments as $at) {
-                if ($at['is_attachment'] == 1) {
+                if (isVideo($at['attachment'])) {
+                    $retVal['debug'][] = "Ignore Video Attachment ".$at['filename'];
+                } else ($at['is_attachment'] == 1) {
                     file_put_contents($at['filename'], $at['attachment']);
                     
-					// Fix Image Rotation Start
-					$exif = exif_read_data($at['filename']);
-					if(array_key_exists('Orientation', $exif)) {
-						$ort = $exif['Orientation']; /*STORES ORIENTATION FROM IMAGE */
-						$ort1 = $ort;
-						$exif = exif_read_data($at['filename'], 0, true);
-						if (!empty($ort1)) {
-							$image = imagecreatefromjpeg($at['filename']);
-							$ort = $ort1;
-							switch ($ort) {
-								case 3:
-									$image = imagerotate($image, 180, 0);
-									break;
-							
-								case 6:
-									$image = imagerotate($image, -90, 0);
-									break;
-							
-								case 8:
-									$image = imagerotate($image, 90, 0);
-									break;
-							}
-						}
-						imagejpeg($image,$at['filename'], 90); /*IF FOUND ORIENTATION THEN ROTATE IMAGE IN PERFECT DIMENSION*/
-					} else {
-						$retVal['debug'][] = "Could not change picture orientation ".$at['filename'];
-					}
-					// Fix Image Rotation End
+                    // Fix Image Rotation Start
+                    $exif = exif_read_data($at['filename']);
+                    if(array_key_exists('Orientation', $exif)) {
+                        $ort = $exif['Orientation']; /*STORES ORIENTATION FROM IMAGE */
+                        $ort1 = $ort;
+                        $exif = exif_read_data($at['filename'], 0, true);
+                        if (!empty($ort1)) {
+                            $image = imagecreatefromjpeg($at['filename']);
+                            $ort = $ort1;
+                            switch ($ort) {
+                                case 3:
+                                    $image = imagerotate($image, 180, 0);
+                                    break;
+                            
+                                case 6:
+                                    $image = imagerotate($image, -90, 0);
+                                    break;
+                            
+                                case 8:
+                                    $image = imagerotate($image, 90, 0);
+                                    break;
+                            }
+                        }
+                        imagejpeg($image,$at['filename'], 90); /*IF FOUND ORIENTATION THEN ROTATE IMAGE IN PERFECT DIMENSION*/
+                    } else {
+                        $retVal['debug'][] = "Could not change picture orientation ".$at['filename'];
+                    }
+                    // Fix Image Rotation End
                     
-					
-					$api = new BaseDropbox(new DropboxApp("1", Config::read('secret'), Config::read('access_token')));
+                    
+                    $api = new BaseDropbox(new DropboxApp("1", Config::read('secret'), Config::read('access_token')));
                     
                     $dropboxFile = new DropboxFile($at['filename']);
                     $file = $api->upload($dropboxFile, Config::read('mail_image_directory_current') . $at['filename'], [
@@ -119,27 +141,56 @@ if ($emails) {
                     $retVal['items'][] = $file->getName();
                     
                     unlink($at['filename']);
-					
-					// Move old files Start
-					$api = new BaseDropbox(new DropboxApp("1", Config::read('secret'), Config::read('access_token')));
-					$listFolderContents = $api->listFolder(Config::read('mail_image_directory_current'));
-					
-					//Fetch Items
-					$items = $listFolderContents->getItems();
-					$cache = array();
-					foreach($items as $cu) {
-						$cache[strtotime($cu->getClientModified())] = $cu->getName();
-					}
-					
-					for($i = 0; $i < (count($cache) - Config::read('image_cache')); $i++) {
-						$item = array_values($cache)[$i];
-						$retVal['cleanup'][] = $item;
-						$file = $api->move("/mails/".$item, Config::read('mail_image_directory_archive').$item);
-					}
-					// Move old files End
-					
+                    
+                    // Move old files Start
+                    
+                    $api = new BaseDropbox(new DropboxApp("1", Config::read('secret'), Config::read('access_token')));
+                    $listFolderContents = $api->listFolder(Config::read('mail_image_directory_current'));
+                    
+                    //Fetch Items
+                    $items = $listFolderContents->getItems();
+                    $cache = array();
+                    foreach($items as $cu) {
+                        $cache[strtotime($cu->getClientModified())] = $cu->getName();
+                    }
+                    
+                    for($i = 0; $i < (count($cache) - Config::read('image_cache')); $i++) {
+                        
+                        $item = array_values($cache)[$i];
+                        $destination = Config::read('mail_image_directory_archive').$item;
+                        
+                        try {
+                            $file = $api->move("/mails/".$item, $destination);
+                            $retVal['cleanup'][] = $item;
+                        } catch(\Exception $e) {
+                            try {
+                                $deletedFolder = $api->delete($destination);
+                                $retVal['cleanup'][] = $e->getMessage().". Deleted item manually";
+                            } catch(\Exception $e) {
+                                $retVal['cleanup'][] = "Failed to Deleted item manually ".$item;
+                            }
+                        }
+                    }
+                    
+                    // Move old files End
+                    
                 }
             }
+        }
+        
+        // Write Subject to file
+        if($overview[0]->subject != "") {
+            $subject = iconv_mime_decode(($overview[0]->subject),0, "UTF-8");
+            $from = iconv_mime_decode(($overview[0]->from),0, "UTF-8");
+            
+            $fp = fopen(Config::read('image_subject_file'), 'a');
+            fwrite($fp, date("Y-m-d").";".$from.";".$subject."\n");
+            fclose($fp);
+            
+            $retVal['messages'][] = $subject;
+            //$retVal['debug'][] = $overview[0];
+        } else {
+            print_r($overview);
         }
         
         imap_delete($inbox, $email_number);
@@ -150,24 +201,28 @@ if ($emails) {
 /* close the connection */
 imap_close($inbox);
 
-if ($emails) {
-	// Trigger the Import in the Gallery
-	$retVal['message'] = "Trigger Gallery Refresh.";
-	$options = array(
-		'http' => array(
-			'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-			'method' => 'POST'
-		)
-	);
-	$context = stream_context_create($options);
-	$result = file_get_contents(Config::read('admin_url'), false, $context);
-	// var_dump($result);
+// skipupdate only for testing
+if ($emails && !isset($_GET['skipupdate'])) {
+    // Trigger the Import in the Gallery
+    $retVal['message'] = "Trigger Gallery Refresh.";
+    $options = array(
+        'http' => array(
+            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method' => 'POST'
+        )
+    );
+    $context = stream_context_create($options);
+    try {
+        $result = @file_get_contents(Config::read('admin_url'), false, $context);
+    } catch(Exception $e) {
+        $retVal['debug'][] = "failed to update gallery";
+    }
+    
 } else {
-	$retVal['message'] = "No Update Needed";
+    $retVal['message'] = "No Update Needed";
 }
 
 
 //print_r($cache);
-header('Content-Type: application/json');
 echo json_encode($retVal);
 ?>
